6603da8a7781ef3b128c263aabe9beed8dfa9d69
sarwar-ku <147907694+sarwar-ku@users.noreply.github.com>
2023-11-02 02:25:06 +0600

    Create 1_DL_terminology_training_and_valid_curves.py

---
+++ 1_DL_terminology_training_and_valid_curves.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  4 15:38:54 2023
+
+@author: Sarwar
+
+Understanding deep learning terminology
+
+This code can be used to experiment and explain training and validation curves
+
+cifar10 dataset
+
+60,000 32×32 pixel images divided into 10 classes.
+
+0: airplane
+
+1: automobile
+
+2: bird
+
+3: cat
+
+4: deer
+
+5: dog
+
+6: frog
+
+7: horse
+
+8: ship
+
+9: truck
+"""
